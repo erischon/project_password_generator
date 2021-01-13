12094b23e5a129212b6892ed8478d15511c2919b
Eri Schon <erischon@gmail.com>
2021-01-13 11:28:09 +0100

    Update views.py

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
     return render(request, "generator/home.html")
 
 def password(request):
-    thepassword = 'testing'
-    return render(request, "generator/password.html", {'password':thepassword })
+
+    characters = list("abcdefghijklmnopqrstuvwxy")
+    length = 10
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    
+    return render(request, "generator/password.html", {'password':thepassword})
